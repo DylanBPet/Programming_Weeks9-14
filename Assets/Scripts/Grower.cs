569a668174c2707ea3ea521587411a226b14876e
DylanBPet <dylanpetroff13@gmail.com>
2026-03-16 18:05:53 -0400

    i started

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    public Transform treeTrans;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GrowTree()
+    {
+        float t = 0;
+        t += Time.deltaTime;
+
+    }
+
+}
