324af1326d1cd967c942b97ebdef9dd0c3127e50
DylanBPet <dylanpetroff13@gmail.com>
2026-03-26 23:48:01 -0400

    Zoom in and out feature fully implemented

--- Assets/Systems Assignment/Scripts/ZoomingInAndOut.cs
+++ Assets/Systems Assignment/Scripts/ZoomingInAndOut.cs
@@ -6,13 +6,15 @@ using UnityEngine.InputSystem;
 public class ZoomingInAndOut : MonoBehaviour
 {
     //ZoomInMode variables I will need
-    public GameObject ZoomInMode;
-    public SpriteRenderer zoomInModeSprite;
-    public Animator zoomInModeAnimator;
+    public GameObject ZoomInMode; //The parent game object
+    public SpriteRenderer zoomInModeSprite; //the current sprite renderer
+    public Animator zoomInModeAnimator; //the animator for the planet
+    public GameObject zoomInCanvas; //the canvas
 
     //ZoomOutMode variables I will need
     public GameObject ZoomOutMode;
 
+    
     //A list of all the planets that are around the map
     public List<SpriteRenderer> planetSprites;
 
@@ -31,7 +33,16 @@ public class ZoomingInAndOut : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GoToZoomOutMode();
+        //turn Zoom in mode on just in case the canvas is not turned off itself (this will effect the fade to black effect)
+        ZoomInMode.SetActive(true);
+
+        //turn them back off with the canvas first
+        zoomInCanvas.SetActive(false);
+        ZoomInMode.SetActive(false);
+
+        //turn zoom out mode on
+        inZoomOutMode = true;
+        ZoomOutMode.SetActive(true);
     }
 
     // Update is called once per frame
@@ -50,56 +61,90 @@ public class ZoomingInAndOut : MonoBehaviour
             //Make an arraylist of planet sprites, when clicked, go to zoom in mode and change the zoom in mode sprite to the same sprite as the planet clicked
             for (int i = 0; i < planetSprites.Count; i++)
             {
-                //check if any planet has been clicked by playing using the attack started
-                if (planetSprites[i].bounds.Contains(mousePos) == true)
+                //Only do this when attack context has been started
+                if (context.started == true)
                 {
-                   
-                    //only do it when context is started
-                    if (context.started == true)
-                    {
-                       
-                        //Change InZoomOutMode to false because we are no longer in zoom out mode
-                        inZoomOutMode = false;
-                        Debug.Log("Go to zoom in mode");
-
-                        //Get the sprite from the clicked planet and change the zoom in mode sprite to be the same as it
-                        newPlanetSprite = planetSprites[i].sprite;
-                        zoomInModeSprite.sprite = newPlanetSprite;
-
-                        //change to zoom in mode
-
-                        ZoomOutMode.SetActive(false);
-                        ZoomInMode.SetActive(true);
-
-                        //change the animator int to i - This will be set up in the animator under the zoom in planet sprite and play the correct sprite 
-                        
-
-                        //Have the black screen fade in and out with a coroutine
-
-                        zoomInModeAnimator.SetInteger("PlanetSpriteNumber", i);
-                    }
+                    //run this function that checks through the array to see if you clicked inside a planet. If yes, change to zoom in mode
+                    StartCoroutine (CheckTheList(i));
                 }
             }
         }
     }
 
-    public void GoToZoomOutMode()
+    IEnumerator CheckTheList(int i)
     {
-        //this will go on a button
-        inZoomOutMode = true;
-        ZoomOutMode.SetActive(true);
-        ZoomInMode.SetActive(false);
+        //check if any planet has been clicked by playing using the attack started
+        if (planetSprites[i].bounds.Contains(mousePos) == true)
+        {
+                //Change InZoomOutMode to false because we are no longer in zoom out mode
+                inZoomOutMode = false;
+
+                //Get the sprite from the clicked planet and change the zoom in mode sprite to be the same as it
+                newPlanetSprite = planetSprites[i].sprite;
+                zoomInModeSprite.sprite = newPlanetSprite;
+
+
+            //make the screen fade effect & wait for it to finish+
+            alpha = 0;
+            while (alpha <= 1)
+            {
+                blackOverlay.color = new Color(0, 0, 0, alpha);
+                alpha += 0.005f;
+                yield return null;
+            }
+
+            //change to zoom in mode
+            ZoomOutMode.SetActive(false);
+            ZoomInMode.SetActive(true);
+            //change the animator int to i - This will be set up in the animator under the zoom in planet sprite and play the correct sprite 
+            zoomInModeAnimator.SetInteger("PlanetSpriteNumber", i);
+
+            //Have the black screen fade in and out with a coroutine (have it hold on a black screen for a few frames)
+            alpha = 1.1f;
+            while (alpha >= 0)
+            {
+                blackOverlay.color = new Color(0, 0, 0, alpha);
+                alpha -= 0.003f;
+                yield return null;
+            }
+            zoomInCanvas.SetActive(true);
+        }
     }
 
-    IEnumerator fadeIn()
+    public void ZoomOutCoroutine()
     {
-        blackOverlay.color = new Color(1, 1, 1, alpha);
-        yield return null;
+        //this will go on a button
+        StartCoroutine(GoToZoomOutMode());
     }
-    IEnumerator fadeout()
+
+    IEnumerator GoToZoomOutMode()
     {
-        blackOverlay.color = new Color(1, 1, 1, alpha);
-        yield return null;
+        //turn off the canvas
+        inZoomOutMode = true;
+        zoomInCanvas.SetActive(false);
+
+        //fade to black
+        alpha = 0;
+        while (alpha <= 1)
+        {
+            blackOverlay.color = new Color(0, 0, 0, alpha);
+            alpha += 0.005f;
+            yield return null;
+        }
+
+        //turn off zoom in mode
+        ZoomInMode.SetActive(false);
+        //Change game object back to zoom out mode
+        ZoomOutMode.SetActive(true);
+
+        //fade back to nothing (hold black for a few frames)
+        alpha = 1.1f;
+        while (alpha >= 0)
+        {
+            blackOverlay.color = new Color(0, 0, 0, alpha);
+            alpha -= 0.003f;
+            yield return null;
+        }
     }
 
 }
